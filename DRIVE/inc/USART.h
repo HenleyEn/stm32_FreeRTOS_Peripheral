9c89d3b889ebddcdf6f784b93f454a02ffe09fce
HenleyEn <790220139@qq.com>
2024-01-23 21:57:01 +0800

    🎉 init(项目初始化):

---
+++ DRIVE/inc/USART.h
@@ -0,0 +1,6 @@
+#ifndef __USART_H
+#define __USART_H
+#include "include.h"
+
+void USART_Config(uint32_t BaudRate);
+#endif
---
+++ USER/include.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "stm32f10x.h"                  // Device header
+
+
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "USART.h"
+
+
+
+
+
+
+
+
